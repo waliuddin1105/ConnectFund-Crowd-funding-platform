76c007e1ccc520fd4233bc220edc7f0275fdee84
waliuddin1105 <waliahmed1105@gmail.com>
2026-03-06 21:41:24 +0500

    added models file and updated init

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -17,6 +17,8 @@ authorizations = {
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:14Nov%402005@localhost:5432/crowdfunding_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
@@ -30,13 +32,13 @@ api = Api(
     security='bearer authorizations' 
 )
 
+
 users_ns = Namespace('Users', description='Data about the users')
 campaigns_ns = Namespace('Campaigns', description="Data about the campaigns")
 donations_ns = Namespace('Donations', description='Data about the donations')
 comments_ns = Namespace('Comments', description="Data about the comments")
 payments_ns = Namespace('Payments', description='Data about the payments')
 updates_ns = Namespace('Updates', description="Data about the updates")
-donations_ns = Namespace('Donations', description='Data about the donations')
 follows_ns = Namespace('Follows', description = 'Data about user follows')
 campaign_updates_ns = Namespace('Campaign Updates', description="Data about the campaign updates")
 admin_reviews_ns = Namespace('Admin Reviews', description = 'Data about admin reviews')
