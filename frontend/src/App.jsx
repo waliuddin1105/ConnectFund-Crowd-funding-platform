cacea52402ccbba26d81ddf15799eb99eba67b83
Sajjadecoder <sajjadahmed102005@gmail.com>
2026-04-22 16:07:40 +0500

    modified App.jsx to support newly added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import AllCampaigns from './pages/AllCampaigns'
+import Home from './pages/Home'
+import CampaignDetails from './pages/CampaignDetails'
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     { path: '/login', element: <Login /> },
     { path: '/register', element: <Register /> },
     { path: '/all-campaigns', element: <AllCampaigns /> },
+    { path: '/', element: <Home /> },
+    { path: '/all-campaigns/:id', element: <CampaignDetails /> },
   ])
 
   return (
